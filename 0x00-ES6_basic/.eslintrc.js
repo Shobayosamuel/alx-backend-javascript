756899e28e709a404b8d0199dec3a6097b89062e
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-06-29 21:21:37 +0000

    fix dependencies

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
         "browser": true,
         "es2021": true
     },
-    "extends": "standard",
+    "extends": "plugin:react/recommended",
     "overrides": [
         {
             "env": {
@@ -21,6 +21,9 @@ module.exports = {
         "ecmaVersion": "latest",
         "sourceType": "module"
     },
+    "plugins": [
+        "react"
+    ],
     "rules": {
     }
 }
