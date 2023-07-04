987e24ef63902402b3f2010008600fe2658c4945
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-07-04 22:22:28 +0000

    feat: tak 0 get list

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+export function getListStudents() {
+    const ans = [
+        { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+        { id: 2, firstName: 'James', location: 'Columbia' },
+        { id: 5, firstName: 'Serena', location: 'San Francisco' }
+  ]
+  return ans
+}
+
