f0926969c9c3dfa689fa2d093d5d62abe7de01b7
Shobayosamuel <102698013+Shobayosamuel@users.noreply.github.com>
2023-07-05 08:06:48 +0100

    Update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,8 @@
 export default function cleanSet(set, startString) {
-    if (startString === "") {
-        return "";
-    }
-    const values = Array.from(set).filter(value => value.startsWith(startString));
-    const ans = values.map((value) => value.slice(startString.length));
-    return ans.join('-');
+  if (startString === "") {
+    return "";
+  }
+  const values = Array.from(set).filter(value => value.startsWith(startString));
+  const ans = values.map((value) => value.slice(startString.length));
+  return ans.join('-');
 }
