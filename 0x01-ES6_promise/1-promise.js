609d03a0ef7303b5b7a00a23bded0f8e1e23c371
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-07-28 13:25:05 +0000

    fix: syntax error

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,9 @@
-export default function getFullResponseFromAPI(success) {
+function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-   if (sucess) {
-     resolve({status:200, body: 'Success'});
-   } else {
-    reject(new Error('The fake API is not working currently'));
-   }
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
   });
 }
