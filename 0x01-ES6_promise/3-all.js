896f06332786dbd4ebc6c40203619ab8403ebf2d
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-07-28 19:32:09 +0000

    feat; Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,7 +4,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   const photo = uploadPhoto();
   const user = createUser();
-  Promise.all([photo, user])
-    .then((response) => { console.log(`${response[0].body} ${response[1].firstName} ${response[1].lastName}`);})
-    .catch(() => { console.log('Signup system offline');})
+  return Promise.all([photo, user])
+    .then((response) => { console.log(`${response[0].body} ${response[1].firstName} ${response[1].lastName}`); })
+    .catch(() => { console.log('Signup system offline'); })
 }
