c9ce63833197d72b0c2f7b836b216077673fc666
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-07-30 03:01:17 +0000

    Implement a Holberton course

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,40 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') throw new TypeError('name must be a string');
+    if (!Number.isInteger(length)) throw new TypeError('length must be a number');
+    if (!Array.isArray(students)) throw new TypeError('students type must be an Array');
+
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') throw new TypeError('Name must be a string');
+    this._name = value;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    if (typeof value !== 'number') throw new TypeError('length must be a number');
+    this._length = value;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(values) {
+    if (!(Array.isArray(values) && values.every((item) => typeof item === 'string'))) {
+      throw new TypeError('Students must be an array of string');
+    }
+    this._students = values;
+  }
+}
