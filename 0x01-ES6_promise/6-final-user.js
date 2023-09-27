c590c36f497a20b9eb7cd77489df46f7cd1d95f6
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-09-27 23:08:43 +0100

    fix: final user

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,8 +1,21 @@
 import signUpUser from '4-user-promise.js';
 import uploadPhoto from '5-photo-reject.js';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  const user = signUpUser();
-  const photo = uploadPhoto();
-  
+export default aasync function handleProfileSignup(firstName, lastName, fileName) {
+  try {
+    const userPromise = signUpUser(firstName, lastName);
+    const photoPromise = uploadPhoto(fileName);
+
+    const [userResult, photoResult] = await Promise.all([userPromise, photoPromise]);
+
+    return [
+      { status: 'fulfilled', value: userResult },
+      { status: 'fulfilled', value: photoResult },
+    ];
+  } catch (error) {
+    return [
+      { status: 'rejected', value: error },
+      { status: 'rejected', value: error },
+    ];
+  }
 }
