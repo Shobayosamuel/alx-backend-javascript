1feeba8a8e8c359f1923fc2d7cb35de87b8f6bcc
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-09-29 00:04:20 +0100

    fix: hoisting

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,12 +1,12 @@
 export default function guardrail(mathFunction) {
-    const arr = [];
-    try {
-        const value = mathFunction();
-        arr.push(value);
-    } catch (error) {
-        arr.push(error.message);
-    } finally {
-        arr.push('Guardrail was processed');
-    }
-    return arr;
+  const arr = [];
+  try {
+    const value = mathFunction();
+    arr.push(value);
+  } catch (error) {
+    arr.push(error.message);
+  } finally {
+    arr.push('Guardrail was processed');
+  }
+  return arr;
 }
