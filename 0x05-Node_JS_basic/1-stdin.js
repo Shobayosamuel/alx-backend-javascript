5343b7aed328c059fc9931d8685f90e26f56f0c8
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-12-30 21:06:10 +0000

    fix task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,13 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
 if (process.stdin.isTTY) {
-  process.stdin.on("writable", (writable) => {
-    process.stdout.write(`Your name is: ${writable.toString()}`);
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
     process.exit();
   });
 } else {
-  process.stdin.on('writable', (writable) => {
-    process.stdout.write(`Your name is: ${writable.toString()}`);
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
     process.exit();
   });
   process.on('exit', () => {
