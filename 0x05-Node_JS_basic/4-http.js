92d2e3be20738f72950ccc9a42efc948b2ae661f
Samuel <shobayosamuel62@gmail.com>
2024-01-04 19:40:54 +0000

    fix err

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,8 +1,14 @@
-const http = require('http')
+const http = require('http');
 
-const app = http.createServer(function (req, res) {
-  res.write('Hello Holberton School!');
-  res.end();
-})
-app.listen(1245);
-module.exports = app
+const host = '127.0.0.1';
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+
+app.listen(port, host, () => {});
+
+module.exports = app;
